c08d7a67f73668a05c71451b9fbee570324b1d1a
NSS\NIPUNI SIRIWARDHANA <nssiriwardhana22@gmail.com>
2024-05-14 20:45:27 +0530

    paypal payment integration

---
+++ src/main/java/com/example/paypalintegrate/PaypalintegrateApplication.java
@@ -0,0 +1,13 @@
+package com.example.paypalintegrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalintegrateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaypalintegrateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paypalintegrate/PaypalintegrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paypalintegrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalintegrateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
